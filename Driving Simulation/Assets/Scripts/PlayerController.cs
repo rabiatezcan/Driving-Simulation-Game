4127f0c4bb0ecf136c74a274ea2d5207626d4904
rabiatezcan <rabiatezcan@hotmail.com>
2021-04-23 18:11:38 +0300

    Implemented car's left/right movement.

--- Driving Simulation/Assets/Scripts/PlayerController.cs
+++ Driving Simulation/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float carSpeed = 20f;
+    float carSpeed = 20.0f;
+    GameObject cam;
+    Vector3 camOffset = new Vector3(0, 5, -8);
+    float turnDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        turnDirection = Input.GetAxis("Horizontal");
         gameObject.transform.Translate(Vector3.forward * Time.deltaTime * carSpeed);
+        gameObject.transform.Translate(Vector3.right * Time.deltaTime * turnDirection);
+    }
+    private void LateUpdate()
+    {
+        CameraPositionControl();
+    }
+    void CameraPositionControl()
+    {
+        cam.transform.position = gameObject.transform.position + camOffset;
     }
 }
